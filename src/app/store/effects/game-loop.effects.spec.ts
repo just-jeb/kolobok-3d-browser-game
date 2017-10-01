ce9a5fb187d2556a838520cfa5b0b9ed9310aa83
Evgeny Barabanov <meltedspark@gmail.com>
2017-10-02 00:53:45 +0300

    Make the test work

--- src/app/store/effects/game-loop.effects.spec.ts
+++ src/app/store/effects/game-loop.effects.spec.ts
@@ -1,9 +1,9 @@
 import {GameLoopEffects} from './game-loop.effects';
 import {Observable} from 'rxjs/Observable';
-import {async, inject, TestBed} from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 import {provideMockActions} from '@ngrx/effects/testing';
 import 'rxjs/add/operator/count';
-import {NEXT_FRAME} from '../actions/game-loop.actions';
+import {nextFrame} from '../actions/game-loop.actions';
 
 describe('Game loop effects', () => {
   let actions: Observable<any>;
@@ -19,11 +19,12 @@ describe('Game loop effects', () => {
   });
 
   it('Should never fire render until the previous render completes',
-    async(inject([GameLoopEffects], (effects) => {
-      actions = Observable.from([NEXT_FRAME, NEXT_FRAME]);
+    inject([GameLoopEffects], (effects: GameLoopEffects) => {
+      actions = Observable.from([nextFrame, nextFrame]);
 
       effects.updateAndRender$.count().subscribe(result => {
-        expect(result).toBe(1);
+        expect(result).toBe(2);
       });
-    })));
+
+    }));
 });
