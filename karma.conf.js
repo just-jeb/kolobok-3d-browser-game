775c1c0624abeb24740ed2c9385b80acbdefc97b
Evgeny Barabanov <meltedspark@gmail.com>
2017-11-03 15:22:51 +0200

    Change to headless chrome in CI

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     customLaunchers: {
       ChromeNoSandbox: {
-        base: 'Chrome',
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     }
