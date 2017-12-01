4e9d6daef2861204a726b5bfceee548ee4045eb5
Evgeny Barabanov <meltedspark@gmail.com>
2017-12-01 18:32:50 +0200

    Fix textures path

--- src/app/rendering/kolobok.render-data.ts
+++ src/app/rendering/kolobok.render-data.ts
@@ -1,8 +1,8 @@
 import {RenderData} from './render-data';
 
 export const kolobokRenderData: RenderData = {
-  diffuseTexture: '/assets/textures/kolobok.jpg',
-  bumpTexture: '/assets/textures/kolobokBump.jpg',
+  diffuseTexture: 'assets/textures/kolobok.jpg',
+  bumpTexture: 'assets/textures/kolobokBump.jpg',
   ambientColor: {r: 0, g: 0, b: 0},
   diffuseColor: {r: 1, g: 1, b: 0},
   emissiveColor: {r: .7, g: .6, b: .1},
